aa31b185078150b3617b1ae80744823d2a065c52
fourtimes <jinojoe@gmail.com>
2021-01-06 11:26:35 +0530

    Remove unwanted files

--- routes/ec2Route.js
+++ routes/ec2Route.js
@@ -12,14 +12,8 @@ router.post("/createInstance", ec2detailsController.postcreateInstance);
 
 
 
-router.post('/new', function (req, res, next) {
-  console.log(req.body);
-  res.locals.connection.query(`INSERT INTO register(firstname,lastname,username,city,state,zip)
-   VALUES('${req.body.FirstName}','${req.body.LastName}','${req.body.Username}','${req.body.City}','${req.body.State}','${req.body.Zip}')`, (error, results, fields) => {
-      if (error) throw error;
-      res.send(JSON.stringify(results));
-  });
-});
+
+
 
 
 
